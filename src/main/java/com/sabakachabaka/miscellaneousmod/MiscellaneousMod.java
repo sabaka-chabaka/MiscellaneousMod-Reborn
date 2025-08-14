a81a655c3e8fc1cdb376fd123918a49e7ff7d1ac
ProgramN128 <gamekimg360@gmail.com>
2025-08-15 00:15:46 +0400

    some unfinished things

--- src/main/java/com/sabakachabaka/miscellaneousmod/MiscellaneousMod.java
+++ src/main/java/com/sabakachabaka/miscellaneousmod/MiscellaneousMod.java
@@ -1,12 +1,29 @@
 package com.sabakachabaka.miscellaneousmod;
 
+import com.sabakachabaka.miscellaneousmod.core.init.BlockInit;
+import com.sabakachabaka.miscellaneousmod.core.init.FoodInit;
+import com.sabakachabaka.miscellaneousmod.core.init.ItemInit;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(value = MiscellaneousMod.MODID)
 public class MiscellaneousMod {
     public static final String MODID = "miscellaneousmod";
 
-    public MiscellaneousMod(){
+    public MiscellaneousMod() {
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        BlockInit.BLOCKS.register(bus);
+        ItemInit.ITEMS.register(bus);
+        FoodInit.FOODS.register(bus);
     }
+
+    public static final CreativeModeTab FOOD_TAB = new CreativeModeTab(MODID + "food_tab") {
+        @Override
+        public ItemStack makeIcon() {
+            return null;
+        }
+    };
 }
--- src/main/java/com/sabakachabaka/miscellaneousmod/core/init/FoodInit.java
+++ src/main/java/com/sabakachabaka/miscellaneousmod/core/init/FoodInit.java
@@ -4,6 +4,7 @@ import com.sabakachabaka.miscellaneousmod.MiscellaneousMod;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class FoodInit {
     public static final DeferredRegister<Item> FOODS = DeferredRegister.create(ForgeRegistries.ITEMS, MiscellaneousMod.MODID);
